1347946d55c2908f32b4bd9b3d7fcda79bd11385
Albin <albinglindell98@gmail.com>
2021-10-13 18:40:41 +0200

    småfix

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let para = document.createElement("h3")
 container.appendChild(para)
 container.appendChild(para1)
 
-
 let handel = [{
     name:"mango",
     price:15,
@@ -31,12 +30,15 @@ handel.forEach(item => {
     let btn= document.createElement("button")
     btn.innerText= "Buy"
     container.appendChild(btn)
+    let para3 = document.createElement("p")
+    para3.innerText = item.price+"$"
+    container.appendChild(para3)
     btn.addEventListener("click", ()=>{
         
         total = total + item.price
         console.log(total)
-        para.innerText="Totalt " + total +" Kr"
+        para.innerText="Total " + total +"$"
     })
 });
-para.innerText="Totalt " + total +" Kr"
+para.innerText="Total " + total +"$"
 
