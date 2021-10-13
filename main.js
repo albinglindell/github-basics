f82b2647579eec671e86f4d7416aed2ff81a055b
Albin <albinglindell98@gmail.com>
2021-10-13 17:08:55 +0200

    en köp funktion

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-let myName = "Albin Lindell";
+let total = 0;
+let container = document.querySelector(".container")
+let para1 = document.createElement("h1")
+let para = document.createElement("h3")
+container.appendChild(para)
+container.appendChild(para1)
 
-console.log(`jag heter ${myname}`)
 
-let friend = "Daigoro"
+let handel = [{
+    name:"mango",
+    price:15,
+    storlek:"large",
+    rutten:false
+},
+{
+    name:"gurka",
+    price:39,
+    storlek:"large",
+    rutten:false
+},
+{
+    name:"potatis",
+    price:49,
+    storlek:"large",
+    rutten:false
+}
+]
+
+handel.forEach(item => {
+
+    let btn= document.createElement("button")
+    btn.innerText= "Buy"
+    container.appendChild(btn)
+    btn.addEventListener("click", ()=>{
+        
+        total = total + item.price
+        console.log(total)
+        para.innerText="Totalt " + total +" Kr"
+    })
+});
+para.innerText="Totalt " + total +" Kr"
+
